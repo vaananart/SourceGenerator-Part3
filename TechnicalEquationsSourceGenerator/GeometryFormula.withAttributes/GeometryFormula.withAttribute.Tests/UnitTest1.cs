a037bf667d7a4af6ac40bc76e73343f9d35b106b
Joseph Saravanan Ganesan <vaananart@gmail.com>
2022-03-11 22:29:11 +1100

    added unit test for the user project of the source generator

--- TechnicalEquationsSourceGenerator/GeometryFormula.withAttributes/GeometryFormula.withAttribute.Tests/UnitTest1.cs
+++ TechnicalEquationsSourceGenerator/GeometryFormula.withAttributes/GeometryFormula.withAttribute.Tests/UnitTest1.cs
@@ -2,16 +2,28 @@ using NUnit.Framework;
 
 namespace GeometryFormula.withAttribute.Tests;
 
+[TestFixture]
 public class Tests
-{
-    [SetUp]
-    public void Setup()
-    {
-    }
-
+{ 
     [Test]
-    public void Test1()
-    {
-        Assert.Pass();
-    }
+    public void SimpleShapeFacadeWithAttributeTest()
+	{ 
+		//Arrange
+		var area = new ShapeFacade();
+
+		//Action
+		var parallelogram = area.ForParallelogram(2, 4);
+		var rectanglArea = area.ForReactangle(4, 2);
+		var squareArea = area.ForSquare(4);
+		var trapezoidArea = area.ForTrapezoid(2, 4, 3);
+		var triangleArea = area.ForTriangle(3, 4);
+
+		//Assert
+		Assert.AreEqual(8, parallelogram);
+		Assert.AreEqual(8, rectanglArea);
+		Assert.AreEqual(16, squareArea);
+		Assert.AreEqual(9, trapezoidArea);
+		Assert.AreEqual(6, triangleArea);
+
+	}
 }
--- TechnicalEquationsSourceGenerator/GeometryFormula.withAttributes/GeometryFormula.withAttribute/ShapeFacade.cs
+++ TechnicalEquationsSourceGenerator/GeometryFormula.withAttributes/GeometryFormula.withAttribute/ShapeFacade.cs
@@ -1,26 +1,22 @@
-﻿// using GeometryFormula.withAttribute.SourceGenerator.Formulas;
-
-
-
-using GeometryFormula.withAttribute.SourceGenerator.Formulas;
+﻿using GeometryFormula.withAttribute.SourceGenerator.Formulas;
 
 namespace GeometryFormula.withAttribute;
 
-// [Shape("Trapezoid", areaFormula="((a + b) / 2.0) * h")]
-// [Shape("Parallelogram", areaFormula = "a * h")]
-// [Shape("Reactangle", areaFormula = "a * b")]
-// [Shape("Triangle", areaFormula="(b * h)/2.0")]
-[Shape("Square")]
+[Shape("Trapezoid", "((a + b) / 2.0) * h")]
+[Shape("Parallelogram", "a * h")]
+[Shape("Reactangle","a * b")]
+[Shape("Triangle", "(b * h)/2.0")]
+[Shape("Square", "a * a")]
 public class ShapeFacade
 {
-	// public double ForParallelogram(double a, double h) => new SourceGenerator.Formulas.Parallelogram(a, h).Area;
+	public double ForParallelogram(double a, double h) => new SourceGenerator.Formulas.Parallelogram(a, h).Area;
 
-	// public double ForReactangle(double a, double b) => new SourceGenerator.Formulas.Reactangle(a, b).Area;
+	public double ForReactangle(double a, double b) => new SourceGenerator.Formulas.Reactangle(a, b).Area;
 
-	// public double ForSquare(double a) => new SourceGenerator.Formulas.Square(a).Area;
+	public double ForSquare(double a) => new SourceGenerator.Formulas.Square(a).Area;
 
-	// public double ForTrapezoid(double a, double b,double h) => new SourceGenerator.Formulas.Trapezoid( a, b, h).Area;
+	public double ForTrapezoid(double a, double b,double h) => new SourceGenerator.Formulas.Trapezoid( a, b, h).Area;
 
-	// public double ForTriangle(double a, double h) => new SourceGenerator.Formulas.Triangle(a, h).Area;
+	public double ForTriangle(double a, double h) => new SourceGenerator.Formulas.Triangle(a, h).Area;
 }
 
